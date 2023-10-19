db7607288d91cd17c532c791d80514da69e0baec
Tobotobo <tobotobo.channel@gmail.com>
2023-10-19 13:24:13 +0900

    /vagrant フォルダのマウント問題を解消

--- Vagrantfile
+++ Vagrantfile
@@ -1,6 +1,11 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "hennersz/nixos-23.05-flakes"
 
+  config.vm.network "private_network", ip: "192.168.33.10"
+
+  config.ssh.username = "vagrant"
+  config.ssh.password = "vagrant"
+
   # NixOSのリビルド
   config.vm.provision "shell", inline: <<-SHELL
     cp "/vagrant/custom-configuration.nix" "/etc/nixos/custom-configuration.nix"
--- custom-configuration.nix
+++ custom-configuration.nix
@@ -1,9 +1,17 @@
 { config, pkgs, ... }:
 
 {
-    virtualisation.docker.enable = true;
-    users.users.vagrant.extraGroups = [ "docker" ];
-    environment.systemPackages = with pkgs; [
-      docker-compose
-    ];
+  # dockerとdocker-composeを有効化
+  # ※vagrantユーザーでコマンド叩けるようにdockerグループに追加
+  virtualisation.docker.enable = true;
+  users.users.vagrant.extraGroups = [ "docker" ];
+  environment.systemPackages = with pkgs; [
+    docker-compose
+  ];
+  # nixos-rebuild switch を実行するとマウントが解除されるため明示的にマウント
+  fileSystems."/vagrant" = {
+    device = "vagrant";
+    fsType = "vboxsf";
+    options = ["rw" "nodev" "relatime" "iocharset=utf8" "uid=1000" "gid=994" "_netdev"];
+  };
 }
